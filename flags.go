da5d21cbdc19d4e4a61a03fae6acaf0cad368869
Matt Chadburn <matt.chadburn@ft.com>
2016-01-28 10:10:12 +0000

    sort flags by alpha

--- flags.go
+++ flags.go
@@ -3,34 +3,64 @@ package main
 import (
   "os"
   "fmt"
+  "sort"
   "github.com/codegangsta/cli"
   "github.com/franela/goreq"
   "encoding/json"
 )
 
-type Response1 struct {
-    Page   int
-    Fruits []string
+type Flags struct {
+	Version		float64
+	Flags		[]Flag
+}
+
+type Flag struct {
+    Name		string
+	Description	string
+    State 		bool
+}
+
+// Sorting data in Go requires you to implement the sort.Interface. This
+// interface requires three simple methods: Len, Less, and Swap.
+
+type ByLength []Flag
+
+func (s ByLength) Len() int {
+	return len(s)
+}
+
+func (s ByLength) Swap(i, j int) {
+    s[i], s[j] = s[j], s[i]
+}
+
+func (s ByLength) Less(i, j int) bool {
+    return s[i].Name < s[j].Name
 }
 
 func main() {
+
   app := cli.NewApp()
-  app.Name = "greet"
-  app.Usage = "fight the loneliness!"
+  app.Name = "next-flags"
   app.Action = func(c *cli.Context) {
     
 	res, _ := goreq.Request{ Uri: "https://next-flags.ft.com/api/v1" }.Do()
-	//fmt.Println(res.Response, err)
 
-	//byt := []byte(`{"num":6.13,"strs":["a","b"]}`)
-	var dat map[string]interface{}
     str, _ := res.Body.ToString()
-	if err := json.Unmarshal([]byte(str), &dat); err != nil {
+	flags := Flags{}
+	if err := json.Unmarshal([]byte(str), &flags); err != nil {
 		panic(err)
 	}    
-	//fmt.Println(dat)
-	version := dat["version"].(float64)
-	fmt.Println(fmt.Sprintf("version: %3.f", version))
+
+	// output
+	fmt.Println(fmt.Sprintf("version: %3.f", flags.Version))
+	fmt.Println(fmt.Sprintf("number of flags: %d", len(flags.Flags)))
+	fmt.Println(" ")
+	
+	sort.Sort(ByLength(flags.Flags))
+
+	for i := 0; i < len(flags.Flags); i++ {
+		fmt.Println(fmt.Sprintf(" %t	%s", flags.Flags[i].State, flags.Flags[i].Name))
+	}
 
   }
 
